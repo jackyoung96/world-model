c89cfad5e8f9e108640620ab66171e4c0445402d
Jaekyung-Cho <jackyoung96@snu.ac.kr>
2022-03-21 14:51:37 +0900

    backup

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
-python train_ppo.py --env CartPole-v0 --randomize --tb_log --gpu 0 &
-python train_ppo.py --env Pendulum-v0 --randomize --tb_log --gpu 1 &
+# python train_ppo.py --env CartPole-v0 --randomize --epoch 5000 --tb_log --gpu 0 &
+# python train_ppo.py --env Pendulum-v0 --randomize --epoch 5000 --tb_log --gpu 1 &
 
-python train_diffdac.py --env CartPole-v0 --randomize --tb_log --gpu 2 &
-python train_diffdac.py --env Pendulum-v0 --randomize --tb_log --gpu 3 &
+python train_diffdac.py --env CartPole-v0 --randomize --epoch 5000 --tb_log --gpu 2 &
+python train_diffdac.py --env Pendulum-v0 --randomize --epoch 5000 --tb_log --gpu 3 &
